675518b798c5f8a2f6b1fdabb4fd4006c8fe82ea
pf4pad <pf4pad@gmail.com>
2022-10-24 13:37:47 +0300

    tabs_create

--- app.js
+++ app.js
@@ -8,15 +8,44 @@
 Vue.createApp({
   data() {
     return {
-      formData: {
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: ''
-      },
-      guests: [],
-      formInvitation: false,
-      btnText: 'Send'
+      content: [
+        {
+          id: 0,
+          title: "Judas",
+          artist: "Lady Gaga",
+          category: ["Trending"],
+        },
+        {
+          id: 1,
+          title: "Paparazzi",
+          artist: "Lady Gaga",
+          category: ["Featured", "Genres"],
+        },
+        {
+          id: 2,
+          title: "Just Dance",
+          artist: "Lady Gaga",
+          category: ["Featured", "Trending"],
+        },
+        {
+          id: 3,
+          title: "Meet Me Halfway",
+          artist: "The Black Eye Peace",
+          category: ["Workout"],
+        },
+        {
+          id: 4,
+          title: "Can't Hold Us",
+          artist: "Macklemore & Ryan Lewis",
+          category: ["Most Played", "Trending"],
+        },
+        {
+          id: 4,
+          title: "Dark Horse",
+          artist: "Katy Perry",
+          category: ["Most Played", "New Releases"],
+        },
+      ]
     }
   },
   computed: {
