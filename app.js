cfa0542d0e6416bedf22485a180ada199d793c8b
pf4pad <pf4pad@gmail.com>
2022-10-24 21:15:34 +0300

    tabs_create2

--- app.js
+++ app.js
@@ -49,31 +49,9 @@ Vue.createApp({
     }
   },
   computed: {
-    fullName() {
-      return this.formData.firstName + ' ' + this.formData.lastName
-    },
-    formReady() {
-
-      return Object.values(this.formData).every(item => item.length > 0)
-    },
 
   },
   methods: {
 
-
-    addNewGuest() {
-      console.log(this.guests);
-      this.guests.push('')
-    },
-    removeGuest(index) {
-      this.guests.splice(index, 1)
-    },
-    getInvitation() {
-      console.log(this.guests);
-      this.formInvitation = true
-      this.guests = this.guests.filter(item => item.length > 0)
-    },
-
-
   }
-}).mount(".homework");
+}).mount(".homework2");
